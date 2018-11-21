28bcc1b98750ce6d0ef040f41cc7adce5632e11f
Truls Morten Hammerstrøm Granli <trhg@sarpsborg.com>
2018-11-21 14:47:39 +0100

    First try

---
+++ Script_Deployment/Script_Deployment/S-Install_Audacity.ps1.ps1
@@ -0,0 +1,17 @@
+﻿####
+#
+#Audacity med Lame og FFmpeg codec
+#
+##############################################################
+
+#Installer Audacity med Lame og FFmpeg codec
+
+Start-Process $PSScriptRoot\audacity-win-2.3.0.exe /silent -Verbose
+Start-Process $PSScriptRoot\Lame_v3.99.3_for_Windows.exe /silent -Verbose
+Start-Process $PSScriptRoot\ffmpeg-win-2.2.2.exe /silent -Verbose
+
+
+
+
+
+
